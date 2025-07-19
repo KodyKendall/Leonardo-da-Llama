6948b84f6d765cf60ca664c70a96c57116d25862
Kody Kendall <kodyckendall@gmail.com>
2025-07-19 10:13:15 -0700

    add auth and organization

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,11 +8,33 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def new
   #   super
   # end
-
+  
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    # Create org (give it a name, e.g. email + "'s Org")
+    org = Organization.create!(name: params[:organization_name] || "#{sign_up_params[:email]}'s Org")
+
+    # Build the user with org
+    build_resource(sign_up_params.merge(organization: org))
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
 
   # GET /resource/edit
   # def edit
