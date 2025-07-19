7a8f149f5a3e8bbb4bfd7e4a76ec63fb99375393
Kody Kendall <kodyckendall@gmail.com>
2025-07-19 10:45:40 -0700

    messages index and route

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,8 +6,35 @@ skip_before_action :authenticate_user!, only: [:inbound_sms]
 skip_before_action :verify_authenticity_token, only: [:inbound_sms]
 
   # GET /messages or /messages.json
+
+  # GET /messages or /messages.json
   def index
-    @messages = current_organization.messages
+    org_number = current_organization.twilio_number
+    if params[:conversation_partner]
+      @conversation_partner = params[:conversation_partner]
+      @messages = current_organization.messages
+        .where(
+          "(sent_to = :org AND sent_from = :partner) OR (sent_from = :org AND sent_to = :partner)",
+          org: org_number,
+          partner: @conversation_partner
+        )
+        .order(:created_at)
+    else
+      org_number_escaped = ActiveRecord::Base.connection.quote(org_number)
+      @message_threads = current_organization.messages
+        .select("
+          CASE WHEN sent_to = #{org_number_escaped} THEN sent_from ELSE sent_to END AS conversation_partner,
+          MAX(created_at) as latest_message_time,
+
+          COUNT(*) as message_count,
+          (SELECT body FROM messages m2 
+           WHERE (m2.sent_to = #{org_number_escaped} AND m2.sent_from = CASE WHEN sent_to = #{org_number_escaped} THEN sent_from ELSE sent_to END) 
+              OR (m2.sent_from = #{org_number_escaped} AND m2.sent_to = CASE WHEN sent_to = #{org_number_escaped} THEN sent_from ELSE sent_to END)
+           ORDER BY m2.created_at DESC LIMIT 1) as body
+        ")
+        .group("conversation_partner")
+        .order("MAX(created_at) DESC")
+    end
   end
 
   # GET /messages/1 or /messages/1.json
